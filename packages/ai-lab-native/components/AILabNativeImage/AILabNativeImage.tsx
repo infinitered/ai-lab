cf4eb8cce08f89f96d12077697b8ffc088ec47cd
kateinkim <kateinkim@gmail.com>
2021-10-06 10:48:43 -0600

    react version lock to 17.0.2

--- packages/ai-lab-native/components/AILabNativeImage/AILabNativeImage.tsx
+++ packages/ai-lab-native/components/AILabNativeImage/AILabNativeImage.tsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '@tensorflow/tfjs-react-native';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { fetch, decodeJpeg } from '@tensorflow/tfjs-react-native';
-import { View, Image } from 'react-native';
+import { View, Image, ImageSourcePropType } from 'react-native';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 import * as tf from '@tensorflow/tfjs';
 
 interface ImageProps {
-  src: string;
+  source: ImageSourcePropType;
 }
 
-export const AILabNativeImage = ({ src, ...props }: ImageProps) => {
+export const AILabNativeImage = ({ source, ...props }: ImageProps) => {
   const [model, setModel] = useState(null);
   const [prediction, setPrediction] = useState(null);
-  let image;
 
   useEffect(() => {
     (async () => {
@@ -23,28 +22,23 @@ export const AILabNativeImage = ({ src, ...props }: ImageProps) => {
       setModel(model);
 
       if (model) {
-        image = src;
-        //@ts-ignore
-        const imageAssetPath = Image.resolveAssetSource(image);
+        const imageAssetPath = Image.resolveAssetSource(source);
         const response = await fetch(
           imageAssetPath.uri,
           {},
           { isBinary: true }
         );
-        const imageData = await response.arrayBuffer();
-
-        //@ts-ignore
+        const imageData = (await response.arrayBuffer()) as Uint8Array;
         const imageTensor = decodeJpeg(imageData);
-        //@ts-ignore
         const prediction = await model.classify(imageTensor);
         setPrediction(prediction);
       }
     })();
-  }, []);
+  }, [source]);
 
   return (
     <View>
-      <Image source={image} />
+      <Image source={source} />
     </View>
   );
 };
