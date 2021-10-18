56ee6d55e0ffe5599fa4a6f86a16c83c3d183dae
kateinkim <kateinkim@gmail.com>
2021-10-18 16:34:23 -0600

    The boundary boxes and the classification is working

--- examples/ai-lab-native-example/android/app/src/main/java/com/example/MainApplication.java
+++ examples/ai-lab-native-example/android/app/src/main/java/com/example/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
--- packages/ai-lab-native/components/AILabNativeImage/AILabNativeImage.stories.tsx
+++ packages/ai-lab-native/components/AILabNativeImage/AILabNativeImage.stories.tsx
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react-native';
 import { AILabNativeImage } from './AILabNativeImage';
 
 storiesOf('AILabNativeImage', module).add('to Storybook', () => (
-  <AILabNativeImage source={require('./beach.jpeg')} />
+  <AILabNativeImage source={require('./dinner.jpg')} />
 ));
