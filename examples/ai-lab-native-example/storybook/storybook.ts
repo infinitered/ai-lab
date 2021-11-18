c13bf1b1540d6d672b64af945056b2c60a6d6eba
kateinkim <kateinkim@gmail.com>
2021-11-18 12:21:49 -0700

    Fixing tf.ready() issue with RN 0.66

--- examples/ai-lab-native-example/storybook/storybook.ts
+++ examples/ai-lab-native-example/storybook/storybook.ts
@@ -11,7 +11,7 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({asyncStorage: null});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
--- packages/ai-lab-native/components/AILabNativeVideo/AILabNativeCamera.tsx
+++ packages/ai-lab-native/components/AILabNativeVideo/AILabNativeCamera.tsx
@@ -3,7 +3,6 @@ import { View, Text, Platform, StyleSheet } from 'react-native';
 import * as tf from '@tensorflow/tfjs';
 import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
 import { Camera } from 'expo-camera';
-import * as Permissions from 'expo-permissions';
 import Canvas from 'react-native-canvas';
 import { CLASSES } from '../labels';
 
@@ -25,7 +24,6 @@ export const AILabNativeCamera = ({
   const [drawingTime, setDrawingTime] = useState(0);
   const [perfProps, setPerfProps] = useState<PerformanceInfo>();
   const [predictionFound, setPredictionFound] = useState(false);
-  const [hasPermission, setHasPermission] = useState<boolean>();
 
   const canvasRef = useRef<Canvas>(null);
   const modelPath =
@@ -40,128 +38,122 @@ export const AILabNativeCamera = ({
   //Fixed output tensor width and height (based on TF model).
   const tensorDims = { width: 350, height: 300 };
 
-  // async function getPermission() {
-  //   const { status } = await Camera.requestPermissionsAsync();
-  //   console.log(`permissions status: ${status}`);
-  //   setHasPermission(status === 'granted');
-  // }
+  // const tensorFlowIt = async (img: tf.Tensor3D, model: tf.GraphModel) => {
+  //   if (!img) {
+  //     return;
+  //   }
+  //   const readyfied = tf.expandDims(img, 0);
+  //   const results = await model.executeAsync(readyfied);
 
-  const tensorFlowIt = async (img: tf.Tensor3D, model: tf.GraphModel) => {
-    if (!img) {
-      return;
-    }
-    const readyfied = tf.expandDims(img, 0);
-    const results = await model.executeAsync(readyfied);
+  //   // Prep Canvas
+  //   const canvas = canvasRef.current!;
+  //   const ctx = canvas.getContext('2d');
+  //   canvas.width = tensorDims.width;
+  //   canvas.height = tensorDims.height;
+  //   ctx.font = '16px sans-serif';
+  //   ctx.textBaseline = 'top';
 
-    // Prep Canvas
-    const canvas = canvasRef.current!;
-    const ctx = canvas.getContext('2d');
-    canvas.width = tensorDims.width;
-    canvas.height = tensorDims.height;
-    ctx.font = '16px sans-serif';
-    ctx.textBaseline = 'top';
+  //   // Get a clean tensor of top indices
+  //   const detectionThreshold = 0.4;
+  //   const iouThreshold = 0.4;
+  //   const maxBoxes = 20;
+  //   const prominentDetection = tf.topk(results[0]);
+  //   const justBoxes = results[1].squeeze<tf.Tensor<tf.Rank.R2>>();
+  //   const justValues = prominentDetection.values.squeeze<
+  //     tf.Tensor<tf.Rank.R1>
+  //   >();
 
-    // Get a clean tensor of top indices
-    const detectionThreshold = 0.4;
-    const iouThreshold = 0.4;
-    const maxBoxes = 20;
-    const prominentDetection = tf.topk(results[0]);
-    const justBoxes = results[1].squeeze<tf.Tensor<tf.Rank.R2>>();
-    const justValues = prominentDetection.values.squeeze<
-      tf.Tensor<tf.Rank.R1>
-    >();
+  //   // Move results back to JavaScript in parallel
+  //   const [maxIndices, scores, boxes] = await Promise.all([
+  //     prominentDetection.indices.data(),
+  //     justValues.array(),
+  //     justBoxes.array(),
+  //   ]);
 
-    // Move results back to JavaScript in parallel
-    const [maxIndices, scores, boxes] = await Promise.all([
-      prominentDetection.indices.data(),
-      justValues.array(),
-      justBoxes.array(),
-    ]);
+  //   // https://arxiv.org/pdf/1704.04503.pdf, use Async to keep visuals
+  //   const nmsDetections = await tf.image.nonMaxSuppressionWithScoreAsync(
+  //     justBoxes, // [numBoxes, 4]
+  //     justValues, // [numBoxes]
+  //     maxBoxes,
+  //     iouThreshold,
+  //     detectionThreshold,
+  //     1 // 0 is normal NMS, 1 is Soft-NMS for overlapping support
+  //   );
+  //   const chosen = await nmsDetections.selectedIndices.data();
 
-    // https://arxiv.org/pdf/1704.04503.pdf, use Async to keep visuals
-    const nmsDetections = await tf.image.nonMaxSuppressionWithScoreAsync(
-      justBoxes, // [numBoxes, 4]
-      justValues, // [numBoxes]
-      maxBoxes,
-      iouThreshold,
-      detectionThreshold,
-      1 // 0 is normal NMS, 1 is Soft-NMS for overlapping support
-    );
-    const chosen = await nmsDetections.selectedIndices.data();
+  //   // Mega Clean
+  //   // tf.dispose([
+  //   //   results[0],
+  //   //   results[1],
+  //   //   model,
+  //   //   nmsDetections.selectedIndices,
+  //   //   nmsDetections.selectedScores,
+  //   //   prominentDetection.indices,
+  //   //   prominentDetection.values,
+  //   //   img,
+  //   //   readyfied,
+  //   //   justBoxes,
+  //   //   justValues,
+  //   // ]);
 
-    // Mega Clean
-    // tf.dispose([
-    //   results[0],
-    //   results[1],
-    //   model,
-    //   nmsDetections.selectedIndices,
-    //   nmsDetections.selectedScores,
-    //   prominentDetection.indices,
-    //   prominentDetection.values,
-    //   img,
-    //   readyfied,
-    //   justBoxes,
-    //   justValues,
-    // ]);
+  //   // Drawing time measuring starts
+  //   // let start = performance.now();
 
-    // Drawing time measuring starts
-    // let start = performance.now();
+  //   for (const detection of chosen) {
+  //     ctx.strokeStyle = '#0F0';
+  //     ctx.lineWidth = 4;
+  //     ctx.globalCompositionOperation = 'destination-over';
+  //     const detectedIndex = maxIndices[detection];
+  //     const detectedClass = CLASSES[detectedIndex];
+  //     const detectedScore = scores[detection];
+  //     const dBox = boxes[detection];
 
-    for (const detection of chosen) {
-      ctx.strokeStyle = '#0F0';
-      ctx.lineWidth = 4;
-      ctx.globalCompositionOperation = 'destination-over';
-      const detectedIndex = maxIndices[detection];
-      const detectedClass = CLASSES[detectedIndex];
-      const detectedScore = scores[detection];
-      const dBox = boxes[detection];
+  //     // No negative values for start positions
+  //     const startY = dBox[0] > 0 ? dBox[0] * tensorDims.height : 0;
+  //     const startX = dBox[1] > 0 ? dBox[1] * tensorDims.width : 0;
+  //     const boxHeight = (dBox[2] - dBox[0]) * tensorDims.height;
+  //     const boxWidth = (dBox[3] - dBox[1]) * tensorDims.width;
+  //     ctx.strokeRect(startX, startY, boxWidth, boxHeight);
 
-      // No negative values for start positions
-      const startY = dBox[0] > 0 ? dBox[0] * tensorDims.height : 0;
-      const startX = dBox[1] > 0 ? dBox[1] * tensorDims.width : 0;
-      const boxHeight = (dBox[2] - dBox[0]) * tensorDims.height;
-      const boxWidth = (dBox[3] - dBox[1]) * tensorDims.width;
-      ctx.strokeRect(startX, startY, boxWidth, boxHeight);
+  //     // Draw the label background.
+  //     ctx.globalCompositionOperation = 'source-over';
+  //     const textHeight = 16;
+  //     const textPad = 4;
+  //     const label = `${detectedClass} ${Math.round(detectedScore * 100)}%`;
+  //     const textWidth = (await ctx.measureText(label)).width;
+  //     ctx.fillStyle = '#0B0';
+  //     ctx.fillRect(startX, startY, textWidth + textPad, textHeight + textPad);
+  //     // Draw the text last to ensure it's on top.
+  //     ctx.fillStyle = '#000000';
+  //     ctx.fillText(label, startX, startY);
+  //   }
+  //   // Drawing time measuring ends
+  //   // setDrawingTime(performance.now() - start);
+  // };
 
-      // Draw the label background.
-      ctx.globalCompositionOperation = 'source-over';
-      const textHeight = 16;
-      const textPad = 4;
-      const label = `${detectedClass} ${Math.round(detectedScore * 100)}%`;
-      const textWidth = (await ctx.measureText(label)).width;
-      ctx.fillStyle = '#0B0';
-      ctx.fillRect(startX, startY, textWidth + textPad, textHeight + textPad);
-      // Draw the text last to ensure it's on top.
-      ctx.fillStyle = '#000000';
-      ctx.fillText(label, startX, startY);
-    }
-    // Drawing time measuring ends
-    // setDrawingTime(performance.now() - start);
-  };
+  // const handleCameraStream = (img: IterableIterator<tf.Tensor3D>) => {
+  //   const loop = async () => {
+  //     const nextImageTensor = await img.next().value;
+  //     if (model) {
+  //       await tensorFlowIt(nextImageTensor, model);
+  //     }
 
-  const handleCameraStream = (img: IterableIterator<tf.Tensor3D>) => {
-    const loop = async () => {
-      const nextImageTensor = await img.next().value;
-      if (model) {
-        await tensorFlowIt(nextImageTensor, model);
-      }
+  //     // if autorender is false you need the following two lines.
+  //     // updatePreview();
+  //     // gl.endFrameEXP();
 
-      // if autorender is false you need the following two lines.
-      // updatePreview();
-      // gl.endFrameEXP();
+  //     requestAnimationFrame(loop);
+  //   };
+  //   loop();
+  // };
 
-      requestAnimationFrame(loop);
-    };
-    loop();
-  };
-
-  useEffect(() => {
-    tf.ready().then(async () => {
-      const model = await tf.loadGraphModel(modelPath);
-      setModel(model);
-    });
-    // getPermission();
-  }, []);
+  // useEffect(() => {
+  //   tf.ready().then(async () => {
+  //     const model = await tf.loadGraphModel(modelPath);
+  //     setModel(model);
+  //   });
+  //   // getPermission();
+  // }, []);
 
   // useEffect(() => {
   //   const setupTFJS = async () => {
@@ -188,7 +180,7 @@ export const AILabNativeCamera = ({
 
   return (
     <View style={styles.cameraView}>
-      <TensorCamera
+      {/* <TensorCamera
         style={styles.camera}
         type={Camera.Constants.Type.back}
         // zoom={0}
@@ -197,13 +189,14 @@ export const AILabNativeCamera = ({
         resizeHeight={textureDims.height}
         resizeWidth={textureDims.width}
         resizeDepth={3}
-        onReady={img => handleCameraStream(img)}
+        onReady={handleCameraStream}
         autorender
         useCustomShadersToResize={false}
-      />
+      /> */}
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   camera: {
     width: 700 / 2,
