70b72b75335367c8ccdf7c901b41700b112efcda
Gant Laborde <gantman@gmail.com>
2021-12-22 12:35:19 -0600

    controls for image now in place

--- examples/ai-lab-example/storybook/AILabImage.stories.tsx
+++ examples/ai-lab-example/storybook/AILabImage.stories.tsx
@@ -1,30 +1,107 @@
 import React from 'react';
 import { AILabImage } from 'ai-lab';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 export default {
-  title: 'Example/AILabImage',
+  title: 'Example/AILabImage/SSD Model',
   component: AILabImage,
+  argTypes: {
+    imageSource: {
+      options: ['dinner', 'cat', 'beach'],
+      control: { type: 'select' },
+    },
+    threshold: {
+      control: { type: 'range', min: 0, max: 1, step: 0.01 },
+    },
+    maxBoxes: {
+      control: { type: 'range', min: 0, max: 50, step: 1 },
+    },
+    iouThreshold: {
+      control: { type: 'range', min: 0, max: 1, step: 0.01 },
+    },
+  },
+} as ComponentMeta<typeof AILabImage>;
+
+const gimmeImage = (src: string) => {
+  switch (src) {
+    case 'dinner':
+      return require('./dinner.jpg');
+      break;
+    case 'cat':
+      return require('./cat.jpeg');
+      break;
+    default:
+      return require('./beach.jpeg');
+  }
 };
 
-// @ts-ignore
-const imageStory = args => {
-  const theImage = require('./dinner.jpg');
+////////////////////////////////////////////////////////////////
+//                  Image Defaults
+////////////////////////////////////////////////////////////////
+
+const imageStory: ComponentStory<typeof AILabImage> = (args, { loaded }) => {
+  //@ts-ignore
+  const theImage = gimmeImage(args.imageSource);
   return (
-    <AILabImage perf={args.perf} src={theImage} style={{ width: '100%' }} />
+    <AILabImage
+      model={loaded.SSDModel}
+      src={theImage}
+      style={{ height: '100%' }}
+    />
   );
 };
-
-export const withAnImage = imageStory.bind({});
+export const withAnImageDefaults = imageStory.bind({});
 // @ts-ignore
-withAnImage.args = {
+withAnImageDefaults.args = {
+  imageSource: 'cat',
+};
+withAnImageDefaults.parameters = {
+  controls: { include: ['imageSource'] },
+};
+
+////////////////////////////////////////////////////////////////
+//                  Image with Params
+////////////////////////////////////////////////////////////////
+const imageParamsStory: ComponentStory<typeof AILabImage> = (
+  args,
+  { loaded }
+) => {
+  //@ts-ignore
+  const theImage = gimmeImage(args.imageSource);
+  return (
+    <AILabImage
+      model={loaded.SSDModel}
+      perf={args.perf}
+      src={theImage}
+      style={{ height: '100%' }}
+      modelInfo={{
+        modelType: 'ssd',
+        threshold: args.threshold,
+        nmsActive: args.nmsActive,
+        maxBoxes: args.maxBoxes,
+      }}
+    />
+  );
+};
+export const withImageAndCustomizedSettings = imageParamsStory.bind({});
+withImageAndCustomizedSettings.args = {
   perf: true,
-  // restrain: false,
-  // modelInfo: {
-  //   model: 'this is the model',
-  //   objectDetection: true,
-  //   labels: ['dog', 'cat'],
-  //   threshold: 0.4,
-  //   max: 20,
-  //   IOU: 0.5,
-  // },
+  imageSource: 'dinner',
+  threshold: 0.4,
+  iouThreshold: 0.5,
+  maxBoxes: 20,
+  nmsActive: true,
+};
+
+withImageAndCustomizedSettings.parameters = {
+  controls: {
+    include: [
+      'threshold',
+      'imageSource',
+      'perf',
+      'nmsActive',
+      'maxBoxes',
+      'iouThreshold',
+    ],
+  },
 };
--- examples/ai-lab-example/storybook/preview.js
+++ examples/ai-lab-example/storybook/preview.js
@@ -1,9 +1,21 @@
+import * as tf from '@tensorflow/tfjs';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+// Load models to feed into all components
+// @ts-ignore
+export const loaders = [
+  async () => ({
+    SSDModel: await tf.loadGraphModel(
+      'https://storage.googleapis.com/tfhub-tfjs-modules/tensorflow/tfjs-model/ssd_mobilenet_v2/1/default/1/model.json'
+    ),
+  }),
+];
